010dd2e842fa48de802ac2a7660a79685fbb03bf
Bruno Galvão <bruno_galvao@outlook.com>
2020-06-25 16:46:44 -0300

    Atualização JS - pegando as informações do JSON

--- assets/json/controle.js
+++ assets/json/controle.js
@@ -1,13 +1,44 @@
 $(document).ready(function(){
-    $("button").click(function(){
+    $("body").html(function(){
 
       $.getJSON("assets/json/conteudo.json", function(result){
 
         $.each(result, function(i, menu){
 
-            $.each(result, function(i, menu){
-                console.log(result[0]);
+            var obj = result[0].menu;
+            var obj2 = result[0].gallery;
+            var qtd = obj.length;
+            var qtd2 = obj2.length;
+
+            console.log(obj); //mostra todo o JSON
+
+            $(function () {
+                var text = "";
+                var i = 0;
+
+                for (let i = 0; i < qtd; ++i) {
+
+                    text += "<li><a href="+obj[i].link+">" + obj[i].name + "</a></li>";
+
+                }
+
+                $("nav > ul").html(text);
             });
+
+            // $(" #menu ").append(
+
+            //     '<p>Nome: <b>' + obj[1].name + '</b> </p>',
+            //     '<p>Link: <b>' + obj[1].link + '</b> </p>',
+                
+
+            // );
+            
+            $('li').attr('src',obj2[0].img);
+
+            $(" #gallery ").append(
+                '<p>link: <b>' + obj2[1].img + '</b> </p>',
+                '<p>link: <b>' + obj2[0].img + '</b> </p>',
+            );
 
         });
 
